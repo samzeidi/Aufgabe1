7bca79dce085e3780befd7aae2fd9e58d22bf522
Sam <ahoura2468@gmail.com>
2025-10-23 12:46:40 +0200

    Aufgabe1

--- src/Main.java
+++
@@ -1,6 +0,0 @@
-cd Pfad/zum/Projektordner        # <-- Navigate to your Java project folder
-git init                         # <-- Initialize git
-git remote add origin git@github.com:<USERNAME>/<REPOSITORY_NAME>.git
-git add .
-git commit -m "Initial commit"
-git push -u origin main
